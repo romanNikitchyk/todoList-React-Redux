9f5f202e09657e9ecdb08570e4014c82006e7495
sssromaz <sssromaz@mail.ru>
2022-09-28 18:02:24 +0300

    Updates

---
+++ precache-manifest.3172c1383f4822702a1ab56dd91895b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df829e74ed8c146455e9ab9ec1e6b13c",
+    "url": "/todoList-React-Redux/index.html"
+  },
+  {
+    "revision": "4fad9bcfc7368b80930c",
+    "url": "/todoList-React-Redux/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "db7dfbc70b30764346b1",
+    "url": "/todoList-React-Redux/static/js/2.a6c0c1f7.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList-React-Redux/static/js/2.a6c0c1f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fad9bcfc7368b80930c",
+    "url": "/todoList-React-Redux/static/js/main.6d55c1fa.chunk.js"
+  },
+  {
+    "revision": "99526c026b58cb0de2d2",
+    "url": "/todoList-React-Redux/static/js/runtime-main.4301fe9c.js"
+  }
+]);
